e4cbe519b231847e3f7f93b066bc1807b1841ad0
Marcus1818787 <48459857+Marcus1818787@users.noreply.github.com>
2022-08-30 14:47:51 +0100

    Debugging

--- src/Main.py
+++ src/Main.py
@@ -102,6 +102,7 @@ class Hand():
         for i in range(4):
             print("Moving finger",i)
             if self.current_state[i] != self.grip_pattern[grip][i]:
+                print("Servo is different")
                 self.moveFinger(self.finger_servo[i], self.grip_pattern[grip][i])
                 print("Servo", self.finger_servo[i], "moved")
                 self.current_state[i] = self.grip_pattern[grip][i]
