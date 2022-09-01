725ae78e63ba1021896dc244b9eebd5f05b15307
Marcus1818787 <48459857+Marcus1818787@users.noreply.github.com>
2022-09-01 10:38:47 +0100

    Created loop to test all functions

--- Examples/GripExamples.py
+++ Examples/GripExamples.py
@@ -3,6 +3,4 @@ from time import sleep
 
 example_hand = Hand()
 # Simply cycle through each of the available grip patterns
-for grip in example_hand.grip_pattern:
-    example_hand.changeGrip(example_hand.grip_pattern[grip])
-    sleep(5)
+example_hand.cycleGrips()
