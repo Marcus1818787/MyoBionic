5ccf2304e1ed44cca10c2945fb48abbe96cd35f9
Marcus1818787 <48459857+Marcus1818787@users.noreply.github.com>
2022-09-02 10:49:25 +0100

    Debugging

--- src/Libs/pyomyo.py
+++ src/Libs/pyomyo.py
@@ -111,7 +111,7 @@ class Packet(object):
 class BT(object):
 	'''Implements the non-Myo-specific details of the Bluetooth protocol.'''
 	def __init__(self, tty):
-		self.ser = serial.Serial(port=tty, baudrate=9600, dsrdtr=1, timeout=0.2)
+		self.ser = serial.Serial(port=tty, baudrate=9600, dsrdtr=1)
 		self.buf = []
 		self.lock = threading.Lock()
 		self.handlers = []
@@ -124,6 +124,8 @@ class BT(object):
 			c = self.ser.read()
 			if not c:
 				return None
+			else:
+				print(c)
 
 			ret = self.proc_byte(ord(c))
 			if ret:
