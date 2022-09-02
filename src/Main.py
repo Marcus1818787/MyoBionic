927013df0118e1f98a787551e0cb666a0ec9610a
Marcus1818787 <48459857+Marcus1818787@users.noreply.github.com>
2022-09-02 12:07:36 +0100

    Added rush current timer to resistance function

--- src/Main.py
+++ src/Main.py
@@ -63,13 +63,14 @@ class Hand():
         limit_reach = False
 
         if open_close == 1:
+            rush_current_timer = time.time()
             while limit_reach == False:
                 # Split the dictionary into a list of keys & values, find the index of the value,
                 #   then reference the key with the same index. This relies on the pinout being correct (servo1-4 = channel0-3)
                 resistor_channel = list(self.finger_servo.keys())[list(self.finger_servo.values()).index(finger)]
                 resistor_value = mcp.read_adc(resistor_channel)
                 print(resistor_value)
-                if (resistor_value > threshold):  # The servo is straining against something, it should stop
+                if (resistor_value > threshold) and ((rush_current_timer-time.time()) > 0.5):  # The servo is straining against something, it should stop
                     pi.set_servo_pulsewidth(finger, 0)
                     time.sleep(servo_delay)
                     pi.set_servo_pulsewidth(finger, 1500)   # Set servo to default position
